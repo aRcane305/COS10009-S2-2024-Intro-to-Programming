88e6459c2dfe1348ecdc319b4aacf4f4ba2782b8
aRcane305 <scha305@icloud.com>
2024-08-19 12:24:40 +1000

    lets commit

--- Test Scripts/test.rb
+++ Test Scripts/test.rb
@@ -1,20 +1,17 @@
-# Declare your variables and assign them values here: 
-integer = 128
-string = "programming is great!"
-boolean = false
-float = 2.25
+class Car
+  attr_accessor :make, :model, :year
+  def initialize(ma, mo, ye)
+  puts "hello"
+  @make = ma
+  @model = mo
+  @year = ye
+  end
+end
 
 
+car1 = Car.new("Toyota" , "Kluger" , 2023)
+car2 = Car.new("BMW" , "Series 5" , 2023)
 
-# Complete the following code
-long_message = "I think " +  string
-sum = 128 + integer
-subtraction = 2.5 - float
-logic = false == boolean
+puts car1.make
 
-# Print out the values of the above 4 variables to the terminal by 
-# completing the following:
-puts "The message is: " + long_message
-puts "The sum is: " + sum.to_s
-puts "The subtraction is: " + subtraction.to_s
-puts "The logic is: " + logic.to_s
+puts car2.model
