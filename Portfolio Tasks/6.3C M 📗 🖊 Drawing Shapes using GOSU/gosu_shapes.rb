ff9c6fe5acbb387b9ddbcebc24af622760bb53fa
aRcane305 <scha305@icloud.com>
2024-09-19 23:03:39 +1000

    v0.3 time to lock in

--- Portfolio Tasks/6.3C M 📗 🖊 Drawing Shapes using GOSU/gosu_shapes.rb
+++ Portfolio Tasks/6.3C M 📗 🖊 Drawing Shapes using GOSU/gosu_shapes.rb
@@ -14,24 +14,38 @@ class DemoWindow < Gosu::Window
 
   def draw
     # see www.rubydoc.info/github/gosu/gosu/Gosu/Color for colours
-    draw_quad(0, 0, 0xff_97ebff, 1280, 0, 0xff_97ebff, 0, 720, 0xff_ffffff, 1280, 720, 0xff_ffffff, ZOrder::BACKGROUND)                                                   # background sky box gradient
-
-    Gosu.draw_rect(120, 103, 700, 394, 0xff_333535, ZOrder::MONITOR)                                                                                                      # monitor frame
-    draw_quad(130, 113, 0xff_b7c6ca, 810, 113, 0xff_767a7b, 130, 487, 0xff_929798, 810, 487, 0xff_b7c6ca, ZOrder::MONITOR)                                                # monitor grey screen background gradient     # monitor support stand
+    # background sky box gradient
+    draw_quad(0, 0, 0xff_97ebff, 1280, 0, 0xff_97ebff, 0, 720, 0xff_ffffff, 1280, 720, 0xff_ffffff, ZOrder::BACKGROUND)
+    # monitor frame
+    Gosu.draw_rect(120, 103, 700, 394, 0xff_333535, ZOrder::MONITOR)
+    # monitor grey screen background gradient
+    draw_quad(130, 113, 0xff_b7c6ca, 810, 113, 0xff_767a7b, 130, 487, 0xff_929798, 810, 487, 0xff_b7c6ca, ZOrder::MONITOR)
+    # monitor support stand
     Gosu.draw_rect(380, 527, 180, 45, 0xff_232525, ZOrder::MONITOR)
     draw_triangle(380, 616, 0xff_232525, 380, 527, 0xff_232525, 440, 527, 0xff_232525, ZOrder::MONITOR)
     draw_triangle(380, 616, 0xff_232525, 380, 527, 0xff_232525, 320, 616, 0xff_232525, ZOrder::MONITOR)
-    Gosu.draw_rect(440, 497, 60, 70, 0xff_242727, ZOrder::MONITOR)                                                                                                        # monitor base vertical support
-
-    Gosu.draw_rect(855, 103, 285, 490, 0xff_111010, ZOrder::CASE)                                                                                                         # computer case
-    Gosu.draw_rect(855, 590, 285, 11, 0xff_272424, ZOrder::CASE)                                                                                                          # case bottom frame
-    draw_quad(855, 601, 0xff_272424, 930, 601, 0xff_272424, 855, 616, 0xff_272424, 923, 616, 0xff_272424, ZOrder::CASE)                                                   # case left leg
-    draw_quad(1065, 601, 0xff_272424, 1140, 601, 0xff_272424, 1072, 616, 0xff_272424, 1140, 616, 0xff_272424, ZOrder::CASE)                                               # case right leg
-    draw_quad(1060, 105, Gosu::Color::RED, 1065, 105, Gosu::Color::YELLOW, 1060, 588, Gosu::Color::GREEN, 1065, 588, Gosu::Color::BLUE, ZOrder::TOP)                      # case led gradient panel
-    Gosu.draw_rect(857, 105, 201, 483, 0xff_2b2929, ZOrder::TOP)                                                                                                          # case window
-    Gosu.draw_rect(1104, 263, 36, 14, 0xff_2b2929, ZOrder::TOP)                                                                                                           # case button outline
-    Gosu.draw_rect(1106, 265, 34, 10, 0xff_111010, ZOrder::TOP)                                                                                                           # case button
-    Gosu.draw_rect(1114, 269 ,20 , 2, Gosu::Color::WHITE, ZOrder::TOP)                                                                                                    # case button led
+    draw_triangle(560 ,616, 0xff_232525, 560, 527, 0xff_232525, 500, 527, 0xff_232525, ZOrder::MONITOR)
+    draw_triangle(560 ,616, 0xff_232525, 560, 527, 0xff_232525, 620, 616, 0xff_232525, ZOrder::MONITOR)
+    # monitor base vertical support
+    Gosu.draw_rect(440, 497, 60, 70, 0xff_242727, ZOrder::MONITOR)
+    # computer case
+    Gosu.draw_rect(855, 103, 285, 490, 0xff_111010, ZOrder::CASE)
+    # case bottom frame
+    Gosu.draw_rect(855, 590, 285, 11, 0xff_272424, ZOrder::CASE)
+    # case left leg
+    draw_quad(855, 601, 0xff_272424, 930, 601, 0xff_272424, 855, 616, 0xff_272424, 923, 616, 0xff_272424, ZOrder::CASE)
+    # case right leg
+    draw_quad(1065, 601, 0xff_272424, 1140, 601, 0xff_272424, 1072, 616, 0xff_272424, 1140, 616, 0xff_272424, ZOrder::CASE)
+    # case led gradient panel
+    draw_quad(1060, 105, Gosu::Color::RED, 1065, 105, Gosu::Color::YELLOW, 1060, 588, Gosu::Color::GREEN, 1065, 588, Gosu::Color::BLUE, ZOrder::TOP)
+    # case window
+    Gosu.draw_rect(857, 105, 201, 483, 0xff_2b2929, ZOrder::TOP)
+    # case button outline
+    Gosu.draw_rect(1104, 263, 36, 14, 0xff_2b2929, ZOrder::TOP)
+    # case button
+    Gosu.draw_rect(1106, 265, 34, 10, 0xff_111010, ZOrder::TOP)
+    # case button led
+    Gosu.draw_rect(1114, 269 ,20 , 2, Gosu::Color::WHITE, ZOrder::TOP)
     # ruby background triangle
     draw_triangle(590, 50, 0xff_8511fa, 280, 50, 0xff_8511fa, 590, 250, 0xff_8511fa, ZOrder::RUBYBACKGROUND)
 
