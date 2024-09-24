440baf9e851e373986ff81c5aaad6c3a41152d75
aRcane305 <scha305@icloud.com>
2024-09-24 19:49:01 +1000

    v0.9 Test 1

--- Portfolio Tasks/Test 1/car.rb
+++ Portfolio Tasks/Test 1/car.rb
@@ -11,6 +11,8 @@ class Car
 	end
 end
 
+# read in the car attributes and assign them to the class
+# car_id is an integer, the rest are strings.
 def read_a_car()
 	car_id = read_integer("Enter car id:")
 	car_manufacturer = read_string("Enter manufacturer:")
@@ -19,6 +21,8 @@ def read_a_car()
 	Car.new(car_id, car_manufacturer, car_model, car_kilometres)
 end
 
+# use an array to call the function read_a_car as many times as car_amount requires
+# use car_amount to control the iterations in the array later on
 def read_cars()
 	car_amount = read_integer("How many cars are you entering:")
 	cars = Array.new()
@@ -32,6 +36,7 @@ def read_cars()
 	return cars
 end
 
+# outputs the car attributes inputted earlier onto the terminal
 def print_a_car(a_car)
 	puts("Id: #{a_car.id}")
 	puts("Manufacturer: #{a_car.manufacturer}")
@@ -39,6 +44,7 @@ def print_a_car(a_car)
 	puts("Kilometres: #{a_car.kilometres}")
 end
 
+# using an array, iterates the above print_a_car function according to the length of the array
 def print_cars(cars)
 	index = 0
 	times = cars.length
