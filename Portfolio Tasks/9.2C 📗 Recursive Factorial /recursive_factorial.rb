a6ddea41dd9324976145f54032240ca451d7d480
aRcane305 <scha305@icloud.com>
2024-09-27 08:51:03 +1000

    v1.5 Updated Task 9.2C

--- Portfolio Tasks/9.2C 📗 Recursive Factorial /recursive_factorial.rb
+++ Portfolio Tasks/9.2C 📗 Recursive Factorial /recursive_factorial.rb
@@ -1,12 +1,26 @@
-# Recursive Factorial
-
-# Complete the following
 def factorial(n)
-
+  # anything to the power of 0 is 1
+  if n == 0
+    return 1
+  else
+    return n * factorial(n - 1)
+  end
 end
 
 def main
-  puts factorial(ARGV[0].to_i)
+  # if the argument is above/below one, prints this error message
+  if ARGV.length != 1
+    puts "Incorrect argument - need a single argument with a value of 0 or more."
+    return
+  end
+
+  n = ARGV[0].to_i
+  # prints if the output is less than one
+  if n < 0
+    puts "Incorrect argument - need a single argument with a value of 0 or more."
+    return
+  end
+  puts factorial(n)
 end
 
 main
