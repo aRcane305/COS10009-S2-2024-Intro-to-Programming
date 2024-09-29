cac004c653310fd62d3d541f95c50e64f7781b20
aRcane305 <scha305@icloud.com>
2024-09-29 16:51:54 +1000

    v2.6 Task 11.4HD

--- Portfolio Tasks/11.4HD C Name Tester/name_tester.c
+++ Portfolio Tasks/11.4HD C Name Tester/name_tester.c
@@ -1,27 +1,42 @@
+// standard C libraries
 #include <stdio.h>
 #include <string.h>
+// custom library for managing user inputs (like input_functions)
 #include "terminal_user_input.h"
 
 #define LOOP_COUNT 60
 
+void print_silly_name(my_string name)
+{
+  int i;
+
+  printf(" %s is a", name.str); // space before %s to fix output formatting,
+  i = 0;
+  while (i < LOOP_COUNT)
+  {
+    printf("  silly"); // double space as requirement
+    i = i + 1;
+  }
+  printf("\nname!"); // prints name! on new line (\n inside, different to ruby)
+}
+
 int main()
 {
+  // defined in line 25 - 28 of terminal_user_input.h
   my_string name;
-  int index;
- 
+
+  // storing the user input as an array called name
   name = read_string("What is your name? ");
+  printf("\nYour name");
 
-  printf("Your name is: %s\n", name.str);
-
-  // Move the following code into a procedure
-  // ie:  void print_silly_name(my_string name)
-
-  for(index=0;index<LOOP_COUNT;index++) {
-
-    printf(" a silly, ");
-
+  // name.str basically is Saad is stored in the array, we can proceed
+  if (strcmp(name.str, "Saad") == 0)
+  {
+    printf(" is an AWESOME name!");
+  } else
+  {
+    print_silly_name(name);
   }
-  printf("\nname!\n");
 
   return 0;
 }
