d85af44de511e77e468dcc84237e4c7d1e4a1540
aRcane305 <scha305@icloud.com>
2024-09-30 23:57:26 +1000

    Revert "v2.9 Optimised rubocop.yml"
    This reverts commit 52ac387f

--- Portfolio Tasks/10.1P Fix it - Sort Records/fix_it.rb
+++ Portfolio Tasks/10.1P Fix it - Sort Records/fix_it.rb
@@ -1,34 +1,29 @@
+# frozen_string_literal: true
 
 class MyRecord
-    attr_accessor :name, :age
-    def initialize(name, age)
-      @name = name
-      # fixed typing error
-      @age = age
-    end
+  attr_accessor :name, :age
+
+  def initialize(name, age)
+    @name = name
+    # fixed typing error
+    @age = age
+  end
 end
 
 # Sort from largest to smallest age depending on direction
 def compare(a, b, direction)
-  result = true
-  if (a > b)
-    result = true
-  else
-    result = false
-  end
+  result = a > b
 
-  if (direction)
-    return result 
-  else
-    return !result
-  end
+  return result if direction
+
+  !result
 end
 
-# sort list according to direction 
+# sort list according to direction
 def sort(arr, direction)
-  item_count = arr.length 
-# Now let's use bubble sort. Swap pairs iteratively as we loop through the
-# array from the beginning of the array to the second-to-last value
+  item_count = arr.length
+  # Now let's use bubble sort. Swap pairs iteratively as we loop through the
+  # array from the beginning of the array to the second-to-last value
   for i in (0..item_count - 2)
     # From arr[i + 1] to the end of the array
     for j in ((i + 1)..item_count - 1)
@@ -40,7 +35,7 @@ def sort(arr, direction)
       end
     end
   end
-  return arr
+  arr
 end
 
 # Read the data from the file and print out each line
@@ -50,54 +45,52 @@ def read(aFile)
   count = aFile.gets
   puts("first line: #{count}")
   # fixed to call the correct function
-  if (is_numeric?(count))
+  if is_numeric?(count)
     count = count.to_i
   else
     count = 0
-    puts "Error: first line of file is not a number"
+    puts 'Error: first line of file is not a number'
   end
 
   index = 0
   # count should be larger than index
-  while (count > index)
+  while count > index
     name = aFile.gets
     age = aFile.gets.to_i
     record = MyRecord.new(name, age)
     people << record
-    puts "Line read: " + name
+    puts "Line read: #{name}"
     index += 1
   end
-  return people
+  people
 end
 
 def print_array(list)
   puts("Printing list: number of elements: #{list.length}")
   i = 0
-  while (i < list.length)
+  while i < list.length
     puts("Name: #{list[i].name} Age: #{list[i].age}")
-    i = i + 1
+    i += 1
   end
 end
 
 # Write data to a file then read it in and print it out
 def main
-  file_name = File.join(__dir__, "data.txt")
-  aFile = File.open(file_name, "r") # open for writing
+  file_name = File.join(__dir__, 'data.txt')
+  aFile = File.open(file_name, 'r') # open for writing
   people = read(aFile)
   aFile.close
 
   sorted_people = sort(people, true)
-  
+
   print_array(sorted_people)
 end
 
 # returns true if a string contains only digits
 def is_numeric?(obj)
-  if /[^0-9]/.match(obj) == nil
-    return false
-  end
-  return true
+  return false if /[^0-9]/.match(obj).nil?
+
+  true
 end
 
 main
-
